8df4ca1ce66884feae0db7ad25593b1936b0f580
Ryan Bennett <ryanbennettvoid@gmail.com>
2018-09-21 16:52:05 -0700

    file io

--- src/io/main.c
+++ src/io/main.c
@@ -1,8 +1,44 @@
 
-#include <syscalls.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void printFile( char *filepath )
 {
+
+  FILE *file = fopen( filepath, "r" );
+
+  if ( !file )
+  {
+    printf( "could not open file: %s\n", filepath );
+    exit( 1 );
+  }
+
+  int c;
+
+  for ( ; (c = fgetc(file)) != EOF; putchar(c) )
+    ;
+
+  fclose( file );
+
+  puts( "\n" );
+
+}
+
+int main( int argc, char *argv[] )
+{
+
+  if ( argc < 2 )
+  {
+    puts( "file path not given\n" );
+    exit( 1 );
+  }
+
+  char *filepath = argv[ 1 ];
+
+  printf( "FILENAME_MAX: %d\n", FILENAME_MAX );
+  printf( "FOPEN_MAX: %d\n\n", FOPEN_MAX );
+
+  printFile( filepath );
 
   return 0;
 }
