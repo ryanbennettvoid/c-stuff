0a37d111b36d8183a7b3d51d3e6a21096ec7ea6d
Ryan Bennett <ryanbennettvoid@gmail.com>
2018-09-23 22:02:10 -0700

    wasm fibonacci

--- src/wasm/func.c
+++ src/wasm/func.c
@@ -1,4 +1,6 @@
 
-int square( int n ) {
-  return n * n;
+int fib( int n ) {
+  if ( n == 0 ) return 0;
+  if ( n == 1 ) return 1;
+  return fib( n - 1 ) + fib( n - 2 );
 }
--- src/wasm/scripts.js
+++ src/wasm/scripts.js
@@ -1,6 +1,12 @@
 
 var filename = 'func.wasm';
-var funcId = '_Z6squarei'; // from intermediate .wat file
+var funcId = 'fib'; // from intermediate .wat file
+
+var jsFib = function( n ) {
+  if ( n == 0 ) return 0;
+  if ( n == 1 ) return 1;
+  return jsFib( n -1 ) + jsFib( n - 2 );
+};
 
 fetch( filename )
   .then( function( res ) {
@@ -13,9 +19,28 @@ fetch( filename )
     return new WebAssembly.Instance( module );
   } )
   .then( function( instance ) {
-    var fn = instance.exports[ funcId ];
-    var result = fn( 7 );
-    console.log( 'result from func: ', result );
+
+    var cFunc = instance.exports[ funcId ];
+    var jsFunc = jsFib;
+    var n = 42;
+
+    // run fibonacci sequence in C
+    const t1 = Date.now();
+    cFunc( n );
+    const t2 = Date.now();
+    const cDur = (t2-t1)/1000;
+    console.log( 'C func done in ' + cDur + 's' );
+
+    // // run fibonacci sequence in JS
+    const t3 = Date.now();
+    jsFunc( n );
+    const t4 = Date.now();
+    const jsDur = (t4-t3)/1000;
+    console.log( 'JS func done in ' + jsDur + 's' );
+
+    const ratio = jsDur/cDur;
+    console.log( 'the C version is ' + ratio.toFixed(3) + 'x faster' );
+
   } )
   .catch( function( err ) {
     console.error( 'could not load wasm file: ', err );
